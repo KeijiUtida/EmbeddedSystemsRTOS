82ac957f1a2aed25460e75d8666497d422a26deb
Keiji Utida <leandro.keiji@gmail.com>
2021-03-17 20:10:59 -0300

    Question6

--- src/tarefas.c
+++ src/tarefas.c
@@ -4,19 +4,27 @@
 
 osThreadId_t thread1_id, thread2_id;
 
+typedef struct leds 
+{
+  uint8_t led_id;
+  uint8_t led_time;
+}leds ;
+
 void thread1(void *arg){
   uint8_t state = 0;
-  if (arg == NULL)
+  struct leds *led_activated;
+  led_activated = arg;
+  if (led_activated->led_id == 1)
     while(1){
       state ^= LED1;
       LEDWrite(LED1, state);
-      osDelay(100);
+      osDelay(led_activated->led_time);
     } // while
   else
     while(1){
       state ^= LED2;
       LEDWrite(LED2, state);
-      osDelay(100);
+      osDelay(led_activated->led_time);
     } // while
     
 } // thread1
@@ -25,9 +33,17 @@ void thread1(void *arg){
 void main(void){
   LEDInit(LED2 | LED1); 
   osKernelInitialize();
-  uint32_t led02[1];
+
+  struct leds led01;
+  struct leds led02;
   
-  thread1_id = osThreadNew(thread1, NULL, NULL);
+  led01.led_id =1;
+  led01.led_time = 100;
+  
+  led02.led_id =2;
+  led02.led_time = 200;
+  
+  thread1_id = osThreadNew(thread1, &led01, NULL);
   thread1_id = osThreadNew(thread1, &led02, NULL);
 
 
